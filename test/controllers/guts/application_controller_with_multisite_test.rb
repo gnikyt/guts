8d3f5bbe654af7e47be6ea52362915e24dc5ccc8
Tyler King <tyler.n.king@outlook.com>
2016-04-11 10:50:02 -0230

    Seperation of multisite tests from main application controller tests

---
+++ test/controllers/guts/application_controller_with_multisite_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+module Guts
+  class ApplicationControllerWithMultisiteTest < ActionController::TestCase
+    setup do
+      @controller = Guts::UsersController.new
+      @routes     = Engine.routes
+    end
+    
+    test "should set current site and current site should be nil" do
+      get :index
+      assert_equal nil, assigns('current_site')
+    end
+
+    test "should set current site with actual site" do
+      @request.host = 'fr.testsite.com'
+      get :index
+      
+      assert_instance_of Guts::Site, assigns('current_site')
+      assert_equal @request.host, assigns('current_site').domain
+    end
+  end
+end
