ab1e892a732ebb63e9a1bb508f554b01fc2beef3
Tyler King <tyler.n.king@gmail.com>
2017-03-31 11:47:59 -0230

    Full test coverage

--- test/policies/guts/application_policy_test.rb
+++ test/policies/guts/application_policy_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 module Guts
   class ApplicationPolicyTest < PolicyTest
-    test 'granted concern' do
+    test 'granted concern tests' do
       user    = guts_users :admin_user
       result  = user.send :grant_resource_string, %i(guts navigation_item)
       result2 = user.send :grant_resource_string, %i(guts type)
@@ -17,6 +17,11 @@ module Guts
       assert_equal 'Guts::User', result4
       assert_equal 'Hash', result5
       assert_equal 'String', result6
+    end
+
+    test 'should grant group access if user can not access' do
+      # regular_user_2 has no access to Type.index, but a group they are a part of, does have access... so they should be allowed in
+      assert Guts::TypePolicy.new(guts_users(:regular_user_2), nil).send(:index?)
     end
   end
 end
