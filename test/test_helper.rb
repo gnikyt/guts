6836dd58a7e74982df17e1844834904affb0f627
Tyler King <tyler.n.king@gmail.com>
2017-04-17 15:28:14 -0230

    Included codecov gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,21 @@
 # Start SimpleCov
 require 'simplecov'
 
-unless ENV['NO_COVERAGE']
-  SimpleCov.start 'rails' do
-    add_group 'Concerns', '/app/concerns'
-    add_group 'Generators', '/lib/generators'
-    add_group 'Policies', '/app/policies'
-    add_filter 'lib/guts/version.rb' # No need to test version file... doesnt work.
-    add_filter 'lib/tasks/guts_tasks.rake' # Inconsistant coverage reports, not sure why
-    add_filter 'lib/tasks/guts_users.rake' # Inconsistant coverage reports, not sure why
-    add_filter 'lib/tasks/guts_db.rake' # Inconsistant coverage reports, not sure why
-    add_filter 'lib/generators/guts/install/install_generator.rb' # Can't find any possible way to test this...
-  end
+SimpleCov.start 'rails' do
+  add_group 'Concerns', '/app/concerns'
+  add_group 'Generators', '/lib/generators'
+  add_group 'Policies', '/app/policies'
+  add_filter 'lib/guts/version.rb' # No need to test version file... doesnt work.
+  add_filter 'lib/tasks/guts_tasks.rake' # Inconsistant coverage reports, not sure why
+  add_filter 'lib/tasks/guts_users.rake' # Inconsistant coverage reports, not sure why
+  add_filter 'lib/tasks/guts_db.rake' # Inconsistant coverage reports, not sure why
+  add_filter 'lib/generators/guts/install/install_generator.rb' # Can't find any possible way to test this...
+end
+
+# Start Codecov
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
 # Configure Rails Environment
